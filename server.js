ae820ff7cce89e927c3a074d8f14b6154c77ae13
joaumvidal <152213388+joaumvidal@users.noreply.github.com>
2023-12-20 17:45:31 +0000

    hiroku app

--- server.js
+++ server.js
@@ -2,14 +2,37 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const data = [
+  {
+    certificationName: 'Salesforce Administrator',
+    domain: 'Salesforce',
+    rewardValue: '30€'
+  },
+  {
+    certificationName: 'Salesforce Developer',
+    domain: 'Salesforce',
+    rewardValue: '100€'
+  },
+  {
+    certificationName: 'AWS Programmer',
+    domain: 'AWS',
+    rewardValue: '74€'
+  },
+  {
+    certificationName: 'MS Certified Admin',
+    domain: 'MS Dynamics',
+    rewardValue: '47€'
+  },
+  {
+    certificationName: 'Azure DevOps',
+    domain: 'Azure',
+    rewardValue:'205€'
+  }
+  // Add more data as needed
+];
 // Define a route that sends a JSON response
-app.get('/api/data', (req, res) => {
-  const jsonData = {
-    message: 'Hello, this is your JSON response!',
-    timestamp: new Date()
-  };
-
-  res.json(jsonData);
+app.get('/api/certifications', (req, res) => {
+  res.json(data);
 });
 
 // Start the server
